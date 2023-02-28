afc0cd0813e3bc93b3f57a1c5fc4b7bb34417b2e
unknown <hruales1138@gmail.com>
2023-02-27 22:35:12 -0500

    Ejercicio 1

--- Ejercicios/01-FindNeedle/FindNeedle.js
+++ Ejercicios/01-FindNeedle/FindNeedle.js
@@ -1,8 +1,25 @@
 function FindNeedle (haystack, needle) {
   // Your code here:
-  return 2
+  let array = [];
+  let j = 0;
+  for (let i = 0; i < haystack.length; i++) {
+    if(haystack[i] === needle[j]) {
+      j = j + 1
+      array.push(i);
+    } 
+    else {
+      j = 0
+      array = []
+    }
+    if(needle.length === array.length) {
+      return array[0]
+    }
+  }
+  return -1
 }
 
-console.log(FindNeedle())
+console.log(FindNeedle('react-redux', 'redux'))
+console.log(FindNeedle('react-redux', 'react'))
+console.log(FindNeedle('react-redux', 'perro'))
 
 module.exports = FindNeedle
