b105864f2448a2ecc6c91f560160031b050eec3f
unknown <hruales1138@gmail.com>
2023-02-28 19:32:25 -0500

    Ejercicio 2

--- Ejercicios/02-SumArray/SumArray.js
+++ Ejercicios/02-SumArray/SumArray.js
@@ -20,10 +20,19 @@ module.exports = SumArray
 //-------------------------------- Solucion 2 --------------------------------
 
 function SumArray2 (arr, n) {
+  let start = 0;
+  let end = arr.length - 1
+  while(start !== end) {
+    let sum = arr[start] + arr[end]
+    console.log(sum)
+    if(sum === n) return true
+    if(sum > n) end --
+    else start ++
+  }
   return false
 }
 
 console.log(SumArray2([2,4,5,9], 9))
 console.log(SumArray2([2,4,5,9], 12))
 console.log(SumArray2([2,4,5,9], 4))
-console.log(SumArray2([2,4,5,9], 4))
+console.log(SumArray2([2,4,5,2], 4))
