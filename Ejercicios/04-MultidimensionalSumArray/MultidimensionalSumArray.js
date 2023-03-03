976b112c33f61a8e7fbaf5a3a53f27626d368774
unknown <hruales1138@gmail.com>
2023-03-02 20:45:24 -0500

    Ejercicio 4

--- Ejercicios/04-MultidimensionalSumArray/MultidimensionalSumArray.js
+++ Ejercicios/04-MultidimensionalSumArray/MultidimensionalSumArray.js
@@ -1,6 +1,15 @@
-function mdArraySum (arr) {
+function mdArraySum(arr) {
   // Your code here:
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if(typeof arr[i] === 'number') sum += arr[i]
+    else sum += mdArraySum(arr[i])
+  }
+  return sum
 }
+
+console.log(mdArraySum([1,2,3,4])) // 10
+console.log(mdArraySum([ [2,4] , [1], [4,2,1] ])) // 14
+console.log(mdArraySum([ 2, [3,4], 5, [-3, [6 , [ 4,5 ] ] ] ])) // 26
 
 module.exports = mdArraySum
