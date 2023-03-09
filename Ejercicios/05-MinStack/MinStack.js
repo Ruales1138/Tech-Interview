588899af55a9c5f4d4fcdbdf00b38d9c7896aef6
unknown <hruales1138@gmail.com>
2023-03-08 19:47:11 -0500

    Ejercicio 5

--- Ejercicios/05-MinStack/MinStack.js
+++ Ejercicios/05-MinStack/MinStack.js
@@ -1,17 +1,29 @@
-class Node {
+class Stack {
   constructor() {
     this.top = null;
   }
 }
 
-class MinStack {
+class Node {
   constructor(value) {
     this.value = value;
     this.next = null;
   }
 }
 
+Stack.prototype.push_stack = function(value) {
+  if(!this.top) {
+    this.top = new Node(value)
+  }
+}
+
+const test_stack = new Stack()
+test_stack.push_stack(32)
+test_stack.push_stack(5)
+test_stack.push_stack(7)
+console.log(test_stack)
+
 module.exports = {
   Node,
-  MinStack
+  Stack
 }
