0ca70698008505e5e24b71192da5b471d8faa5de
unknown <hruales1138@gmail.com>
2023-03-09 21:32:01 -0500

    Ejercicio 5

--- Ejercicios/05-MinStack/MinStack.js
+++ Ejercicios/05-MinStack/MinStack.js
@@ -1,6 +1,7 @@
 class Stack {
   constructor() {
     this.top = null;
+    this.minNum = Infinity;
   }
 }
 
@@ -11,9 +12,10 @@ class Node {
   }
 }
 
-Stack.prototype.push_stack = function(value) {
+Stack.prototype.push= function(value) {
+  if(value < this.minNum) this.minNum = value;
   if(!this.top) {
-    this.top = new Node(value)
+    this.top = new Node(value);
   }
   else {
     let newInfo = new Node(value);
@@ -21,13 +23,34 @@ Stack.prototype.push_stack = function(value) {
     this.top = newInfo;
     newInfo.next = oldInfo;
   }
-}
+};
+
+Stack.prototype.pop = function() {
+  if(!this.top.next) return this.top.value;
+  else {
+    let oldInfo = this.top;
+    this.top = this.top.next
+    return oldInfo.value
+  }
+};
+
+Stack.prototype.min = function() {
+  return this.minNum;
+};
+
+Stack.prototype.peek = function() {
+  return this.top.value;
+};
 
 const test_stack = new Stack()
-test_stack.push_stack(32)
-test_stack.push_stack(5)
-test_stack.push_stack(7)
+test_stack.push(32)
+test_stack.push(5)
+test_stack.push(7)
 console.log(test_stack)
+console.log(test_stack.pop())
+console.log(test_stack)
+console.log(test_stack.min())
+console.log(test_stack.peek())
 
 module.exports = {
   Node,
