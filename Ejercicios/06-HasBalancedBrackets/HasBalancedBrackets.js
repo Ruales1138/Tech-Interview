18b6625a89140d7a03dcfecb5d7d0b18c35172be
unknown <hruales1138@gmail.com>
2023-03-10 22:17:07 -0500

    Ejercicio 6

--- Ejercicios/06-HasBalancedBrackets/HasBalancedBrackets.js
+++ Ejercicios/06-HasBalancedBrackets/HasBalancedBrackets.js
@@ -1,6 +1,10 @@
 function HasBalancedBrackets (string) {
-  // Your code here:
-
 }
+
+console.log(HasBalancedBrackets("{ [ ] ( ) }"))
+console.log(HasBalancedBrackets("{ [ ( ] ) }"))
+console.log(HasBalancedBrackets("{ [ ] ) }"))
+console.log(HasBalancedBrackets("{ [ }"))
+console.log(HasBalancedBrackets("{ [ ( [ { ( )[ ]{ } } ] ) ] }"))
 
 module.exports = HasBalancedBrackets
